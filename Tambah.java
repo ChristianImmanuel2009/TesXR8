59dbe4d8cb6ca4fcdd2ad07e8a542c2a605218d0
ChristianImmanuel2009 <christian_immanuel_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 14:58:41 +0700

    Add files via upload

---
+++ Tambah.java
@@ -0,0 +1,8 @@
+public class Tambah {
+    public static void main(String[] args) {
+        int a = 1000;
+        int b = 2500;
+        int hasil = a+b;
+        System.out.println(" Hasil dari " + a + " ditambah " + b + " adalah " + hasil);
+    }
+}
