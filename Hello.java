b07a9d0b4d064d62e8305af5673ae1f091a2b536
ChristianImmanuel2009 <christian_immanuel_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:01:20 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,29 @@
+
+
+public class Hello{
+    public static void main(String[] args) {
+        
+        String nama= "Christian Immanuel";
+        String namaPanggilan= "Chris";
+        String asalKota="Malang";
+        String hobbi="Melukis";
+        int usia=15;
+        double beratBadan=74.00;
+        float tinggiBadan=170.0f;
+        char jenisKelamin = 'L';
+        boolean status = true;
+
+        System.out.println("Nama: "+nama);
+        System.out.println("Nama Panggilan: "+namaPanggilan);
+        System.out.println("Usia: "+usia);
+        System.out.println("Hobby: "+hobbi);
+        System.out.println("Asal Kota: "+asalKota);
+        System.out.println("Jenis Kelamin: "+jenisKelamin);
+        System.out.println("Berat Badan: "+beratBadan);
+        System.out.println("Berat Badan: "+tinggiBadan);
+        System.out.println("Status: "+status);
+        
+
+    }
+
+}
